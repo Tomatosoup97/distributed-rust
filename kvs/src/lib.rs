74bfd99eb00900a61dbbc8064c910a7a70d1ace7
Mateusz Urbańczyk <mu@qed.ai>
2023-05-19 17:13:48 +0200

    In-memory key/value store

--- kvs/src/lib.rs
+++ kvs/src/lib.rs
@@ -1,20 +1,40 @@
+#![deny(missing_docs)]
 #![allow(dead_code, unused_variables)]
+//! A simple key/value store library.
+use std::collections::HashMap;
 
-pub struct KvStore {}
+/// A key-value store.
+#[derive(Debug)]
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
 impl KvStore {
+    /// Create a new KvStore.
     pub fn new() -> KvStore {
-        todo!()
+        KvStore {
+            map: HashMap::new(),
+        }
     }
+
+    /// Set the value of a string key to a string.
     pub fn set(&mut self, key: String, value: String) {
-        todo!()
+        self.map.insert(key, value);
     }
 
+    /// Get the value of a given key.
     pub fn get(&self, key: String) -> Option<String> {
-        todo!()
+        self.map.get(&key).cloned()
     }
 
+    /// Remove a given key from the store.
     pub fn remove(&mut self, key: String) {
-        todo!()
+        self.map.remove(&key);
+    }
+}
+
+impl Default for KvStore {
+    fn default() -> Self {
+        Self::new()
     }
 }
