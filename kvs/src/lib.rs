d8d2f9f898ded1bf7f8413043d6e1253eec0302d
Mateusz Urbańczyk <mu@qed.ai>
2023-05-19 15:39:05 +0200

    Initial commit

---
+++ kvs/src/lib.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code, unused_variables)]
+
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> KvStore {
+        todo!()
+    }
+    pub fn set(&mut self, key: String, value: String) {
+        todo!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        todo!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        todo!()
+    }
+}
