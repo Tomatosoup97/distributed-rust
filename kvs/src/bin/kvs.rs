548634342bdbe1c479f5612d060d8c2631b9d9a2
Mateusz Urbańczyk <mu@qed.ai>
2023-05-19 17:13:37 +0200

    CLI

--- kvs/src/bin/kvs.rs
+++ kvs/src/bin/kvs.rs
@@ -1,3 +1,37 @@
+#![allow(dead_code, unused_variables)]
+use clap::{Parser, Subcommand};
+use std::process::exit;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    println!("{:?}", cli);
+
+    match cli.command {
+        Command::Get { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Command::Set { key, value } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Command::Rm { key } => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+    }
 }
